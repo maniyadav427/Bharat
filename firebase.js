4be5d1762aa00f3c98fbf9f646664859d9689522
maniyadav427 <25e11a66h0@biet.ac.in>
2026-08-09 13:58:59 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,10 @@
 // Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+
 import {
     getAuth,
     createUserWithEmailAndPassword,
@@ -10,33 +15,80 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
 
+import {
+    getFirestore,
+    doc,
+    setDoc
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+
+
 // Firebase configuration
+
 const firebaseConfig = {
+
     apiKey: "AIzaSyAXAoP4ZAurROURN_ThBmiS5se7B2_foI4",
+
     authDomain: "allied-f7854.firebaseapp.com",
+
     projectId: "allied-f7854",
+
     storageBucket: "allied-f7854.firebasestorage.app",
+
     messagingSenderId: "754424931220",
+
     appId: "1:754424931220:web:97d9a44b7da4c7407ee581",
+
     measurementId: "G-N5M1C672D0"
+
 };
 
 
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
+const app =
+    initializeApp(firebaseConfig);
+
 
 
 // Initialize Authentication
-const auth = getAuth(app);
+
+const auth =
+    getAuth(app);
 
 
-// Make Firebase available to other JavaScript modules
+
+// Initialize Firestore Database
+
+const db =
+    getFirestore(app);
+
+
+
+// Make Firebase available
+// to other JavaScript modules
+
 export {
+
     app,
+
     auth,
+
+    db,
+
+    doc,
+
+    setDoc,
+
     createUserWithEmailAndPassword,
+
     sendEmailVerification,
+
     signInWithEmailAndPassword,
+
     signOut,
+
     onAuthStateChanged
+
 };
